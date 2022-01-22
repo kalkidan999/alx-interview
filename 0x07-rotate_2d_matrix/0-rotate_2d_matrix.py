3847b93548823c43b57f23f1f0f147d1f20a8034
kalkidan Demes <Kalkidan.Demes@belcash.com>
2022-01-22 12:34:15 +0300

    rotate 90 degree

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,3 +2,19 @@
 """
 Rotate 2D Matrix
 """
+
+
+from operator import length_hint
+
+
+def rotate_2d_matrix(matrix):
+    """n x n 2D matrix, rotate it
+    90 degrees clockwise"""
+    Len = length_hint(matrix[0])
+    for i in range(Len // 2):
+        for j in range(i, Len - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[Len - 1 - j][i]
+            matrix[Len - 1 - j][i] = matrix[Len - 1 - i][Len - 1 - j]
+            matrix[Len - 1 - i][Len - 1 - j] = matrix[j][Len - 1 - i]
+            matrix[j][Len - 1 - i] = temp
